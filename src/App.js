2a499d8f284162babee6fe3ff33659bab382a1eb
caelumpirata <caelumpirata@gmail.com>
2022-04-17 12:39:03 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          peronal website bitch ! thanks to visit
-        </p>
-        <a
-          className="App-link"
-          href="https://caelumpirata.github.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Clikc for more !
-        </a>
-      </header>
+      <h1>This is my website homepage !</h1>
     </div>
   );
 }
